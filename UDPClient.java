cf9412e67edf33cf654dd7a01fbb8c37809fe9c4
David-Brodsky <73510062+David-Brodsky@users.noreply.github.com>
2023-11-08 22:52:41 -0600

    Added log, open connections, basic protocol

--- UDPClient.java
+++ UDPClient.java
@@ -1,35 +1,43 @@
 import java.io.*; 
 import java.net.*; 
   
-class UDPClient { 
-    public static void main(String args[]) throws Exception 
-    { 
-      BufferedReader inFromUser = 
-        new BufferedReader(new InputStreamReader(System.in)); 
-      DatagramSocket clientSocket = new DatagramSocket(); 
-  
-      InetAddress IPAddress = InetAddress.getByName("127.0.0.1"); 
-  
-      byte[] sendData = new byte[1024]; 
-      byte[] receiveData = new byte[1024]; 
-  
-      String sentence = inFromUser.readLine(); 
-      sendData = sentence.getBytes();         
+class UDPClient {
+	public static void main(String args[]) throws Exception {
+		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+		DatagramSocket clientSocket = new DatagramSocket();
 
-      DatagramPacket sendPacket = 
-    	         new DatagramPacket(sendData, sendData.length, IPAddress, 9876); 
-    	  
-    	      clientSocket.send(sendPacket); 
-    	  
-    	      DatagramPacket receivePacket = 
-    	         new DatagramPacket(receiveData, receiveData.length); 
-    	  
-    	      clientSocket.receive(receivePacket); 
-    	  
-    	      String modifiedSentence = 
-    	          new String(receivePacket.getData()); 
-    	  
-    	      System.out.println("FROM SERVER:" + modifiedSentence); 
-    	      clientSocket.close(); 
-    	      } 
-    	}
+		InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
+
+		boolean connectionEstablished = false;
+
+		while(true) {
+			byte[] sendData;
+			byte[] receiveData = new byte[1024];
+
+			if(connectionEstablished)
+				System.out.println("Enter your mathematical question or STOP to terminate the connection.");
+			else
+				System.out.println("Please enter your username to connect to the server.");
+
+			String sentence = inFromUser.readLine();
+			if(sentence.equals("STOP"))
+				break;
+
+			sendData = sentence.getBytes();
+
+			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+			clientSocket.send(sendPacket);
+
+			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			clientSocket.receive(receivePacket);
+
+			String modifiedSentence = new String(receivePacket.getData());
+			System.out.println("FROM SERVER:" + modifiedSentence);
+
+			connectionEstablished = true;
+		}
+
+		System.out.println("Terminating connection with the server");
+		clientSocket.close();
+	}
+}
--- UDPServer.java
+++ UDPServer.java
@@ -1,39 +1,64 @@
+import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.*;
+import java.util.logging.*;
+import java.util.*;
+
 
 class UDPServer {
     public static void main(String args[]) throws Exception
     {
-
         DatagramSocket serverSocket = new DatagramSocket(9876);
+        HashMap<InetAddress, String> nameMap = new HashMap<InetAddress, String>();
 
-        byte[] receiveData = new byte[1024];
-        byte[] sendData  = new byte[1024];
+        Logger logger = Logger.getLogger("MyLog");
+        FileHandler fileHandler = new FileHandler("server.log");
+        SimpleFormatter formatter = new SimpleFormatter();
+        fileHandler.setFormatter(formatter);
+        logger.addHandler(fileHandler);
 
-        System.out.println("Server running!");
+
+        logger.info("Server running!");
 
         while(true)
         {
+            byte[] receiveData = new byte[1024];
+            byte[] sendData;
 
-            DatagramPacket receivePacket =
-                    new DatagramPacket(receiveData, receiveData.length);
+            DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
 
-            String sentence = new String(receivePacket.getData());
-
+            String sentence = new String(receivePacket.getData()).trim();
             InetAddress IPAddress = receivePacket.getAddress();
-
             int port = receivePacket.getPort();
 
-            String capitalizedSentence = sentence.toUpperCase();
+            String user = nameMap.get(IPAddress);
+            if(user == null) {
+                nameMap.put(IPAddress, sentence);
+                logger.info("Connection established with IP Address " + IPAddress + " with client name " + sentence);
 
-            sendData = capitalizedSentence.getBytes();
+                String ack = "Connection with server successfully established";
+                sendData = ack.getBytes();
 
-            DatagramPacket sendPacket =
-                    new DatagramPacket(sendData, sendData.length, IPAddress,
-                            port);
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                serverSocket.send(sendPacket);
 
-            serverSocket.send(sendPacket);
+                logger.info("Sent acknowledgement to " + sentence);
+            }
+
+            else {
+                logger.info("User " + user + " sent the following message to port #" + port + "\n" + sentence);
+
+                String response = "Temp response";
+                sendData = response.getBytes();
+
+                DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
+                serverSocket.send(sendPacket);
+
+                logger.info("Sent the following response to " + user + "\n" + sentence);
+            }
+
+
         }
     }
 }
