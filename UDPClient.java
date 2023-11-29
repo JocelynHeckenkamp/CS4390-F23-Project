76b9f0e167437996be54a300630e73fd7f6f89ac
David-Brodsky <73510062+David-Brodsky@users.noreply.github.com>
2023-11-28 19:33:31 -0600

    Added comments and cleaned up code

--- UDPClient.java
+++ UDPClient.java
@@ -1,42 +1,54 @@
 import java.io.*; 
 import java.net.*; 
-  
+
+// NOTE: This file is based on the UDPClient file given to us in class
 class UDPClient {
 	public static void main(String args[]) throws Exception {
+		// creates buffered reader, datagram socket, & IPAddress
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		DatagramSocket clientSocket = new DatagramSocket();
-
 		InetAddress IPAddress = InetAddress.getByName("127.0.0.1");
 
+		// boolean variable that monitors if the connection was successfully established
 		boolean connectionEstablished = false;
 
 		while(true) {
 			byte[] sendData;
 			byte[] receiveData = new byte[1024];
 
+			// asks the user for a mathematical expression or for a username
 			if(connectionEstablished)
 				System.out.println("Enter your mathematical question or STOP to terminate the connection.");
 			else
 				System.out.println("Please enter your username to connect to the server.");
 
+			// reads line and sends packet to server
 			String sentence = inFromUser.readLine();
-			if(sentence.equals("STOP"))
-				break;
-
 			sendData = sentence.getBytes();
-
 			DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
 			clientSocket.send(sendPacket);
 
+			// if user wants to stop, do not wait for server response
+			// send message anyway to remove user from the server
+			if(sentence.equals("STOP"))
+				break;
+
+			// reads answer
 			DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
 			clientSocket.receive(receivePacket);
+			String answer = new String(receivePacket.getData()).trim();
 
-			String modifiedSentence = new String(receivePacket.getData());
-			System.out.println("FROM SERVER:" + modifiedSentence);
-
-			connectionEstablished = true;
+			// outputs the mathematical answer or a confirmation of connection establishment
+			if(connectionEstablished)
+				System.out.println("Answer: " + answer);
+			else {
+				System.out.println(answer);
+				// only establish the connection if the server confirms the username is not in use
+				connectionEstablished = answer.equals("Connection with server successfully established");
+			}
 		}
 
+		// output a message and close the socket
 		System.out.println("Terminating connection with the server");
 		clientSocket.close();
 	}
--- UDPServer.java
+++ UDPServer.java
@@ -1,57 +1,90 @@
-import javax.xml.crypto.Data;
-import java.io.*;
 import java.net.*;
 import java.util.logging.*;
 import java.util.*;
 
-
+// NOTE: This file is based on the UDPServer file given to us in class
 class UDPServer {
     public static void main(String args[]) throws Exception
     {
+        // initializes socket
         DatagramSocket serverSocket = new DatagramSocket(9876);
+
+        // initializes hash map which maps a port number to a username
         HashMap<Integer, String> portMap = new HashMap<Integer, String>();
 
+        // initializes logger
         Logger logger = Logger.getLogger("Server Log");
         FileHandler fileHandler = new FileHandler("server.log");
         SimpleFormatter formatter = new SimpleFormatter();
         fileHandler.setFormatter(formatter);
         logger.addHandler(fileHandler);
-
-
         logger.info("Server running!");
 
-        while(true)
-        {
+        while(true) {
             byte[] receiveData = new byte[1024];
             byte[] sendData;
 
+            // receives UDP packet
             DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
             serverSocket.receive(receivePacket);
 
+            // reads message with trimmed whitespace
             String sentence = new String(receivePacket.getData()).trim();
+
+            // obtains IP address and port number
             InetAddress IPAddress = receivePacket.getAddress();
             int port = receivePacket.getPort();
 
+            // finds username based on port number
+            // user will be null if the port is not in the port map
             String user = portMap.get(port);
+
+            // if user has not connected yet
             if(user == null) {
-                portMap.put(port, sentence);
-                logger.info("Connection established with user " + sentence + "\n" +
-                        "User details: IP Address " + IPAddress + " Port #: " + port);
+                logger.info("User " + sentence + " is attempting to establish a connection with the server\n" +
+                        "User details: IP Address " + IPAddress + " Port Number " + port);
 
-                String ack = "Connection with server successfully established";
-                sendData = ack.getBytes();
+                String response;
 
+                if(portMap.containsValue(sentence)){
+                    logger.info("Connection was not established because username " + sentence + " is already in use");
+                    response = "Username " + sentence + " is already in use. Please enter another username.";
+                }
+
+                else{
+                    portMap.put(port, sentence); // enters the port-username pair to the port map
+                    // logs that the user connected
+                    logger.info("Connection established with user " + sentence);
+
+                    response = "Connection with server successfully established";
+                }
+
+                // sends and acknowledgement message to the client
+                sendData = response.getBytes();
                 DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
                 serverSocket.send(sendPacket);
 
-                logger.info("Sent acknowledgement to " + sentence);
+                // logs that an acknowledgement was sent
+                logger.info("Sent the following response: " + response);
             }
 
+            // user has already connected
             else {
+                // logs the message the user sent
                 logger.info("User " + user + " sent the following message " + "\n" + sentence);
 
-                String response = "nothing";
-                double result = 0;
+                // if the user wishes to terminate the connection
+                if(sentence.equals("STOP")) {
+                    // remove port-username from the port map
+                    // and log that the user disconnected from the server
+
+                    portMap.remove(port);
+                    logger.info("User " + user + " disconnected from the server");
+                    continue;
+                }
+
+                String response;
+                double result;
 
                 try {
                     BinaryTree tree = new BinaryTree(sentence);
@@ -61,11 +94,12 @@ class UDPServer {
                     response = "Parsing error. Valid operations: + - * /";
                 }
 
+                // sends the response to the client
                 sendData = response.getBytes();
-
                 DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, port);
                 serverSocket.send(sendPacket);
 
+                // logs the response that was sent
                 logger.info("Sent the following response to " + user + "\n" + response);
             }
         }
